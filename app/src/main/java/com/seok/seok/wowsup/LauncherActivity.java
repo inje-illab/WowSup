3878a1a3e11bc947ab7cd42129f961e55cda4790
Win_msnodeve <msnodeve@gmail.com>
2019-01-18 23:30:30 +0900

    프래그먼트 작업중

--- app/src/main/java/com/seok/seok/wowsup/LauncherActivity.java
+++ app/src/main/java/com/seok/seok/wowsup/LauncherActivity.java
@@ -25,7 +25,6 @@ public class LauncherActivity extends AppCompatActivity {
         setContentView(R.layout.activity_launcher);
         mHandler.sendEmptyMessageDelayed(0, 500);
         mContext = getApplicationContext();
-        getHashKey(mContext);
     }
 
     private Handler mHandler = new Handler() {
@@ -37,53 +36,30 @@ public class LauncherActivity extends AppCompatActivity {
         }
     };
 
-    // 프로젝트의 해시키를 반환
 
-    @Nullable
-    public static String getHashKey(Context context) {
-
-        final String TAG = "KeyHash";
-
-        String keyHash = null;
-
-        try {
-
-            PackageInfo info =
-
-                    context.getPackageManager().getPackageInfo(context.getPackageName(), PackageManager.GET_SIGNATURES);
-
-
-            for (Signature signature : info.signatures) {
-
-                MessageDigest md;
-
-                md = MessageDigest.getInstance("SHA");
-
-                md.update(signature.toByteArray());
-
-                keyHash = new String(Base64.encode(md.digest(), 0));
-
-                Log.d(TAG, keyHash);
-
-            }
-
-        } catch (Exception e) {
-            Log.e("name not found", e.toString());
-
-        }
-
-
-        if (keyHash != null) {
-
-            return keyHash;
-
-        } else {
-
-            return null;
-
-        }
-
-    }
-
+    // 프로젝트의 해시키를 반환 Kakao 해시키 반환 코드 getHashKey(mContext);
+//    @Nullable
+//    public static String getHashKey(Context context) {
+//        final String TAG = "KeyHash";
+//        String keyHash = null;
+//        try {
+//            PackageInfo info =
+//                    context.getPackageManager().getPackageInfo(context.getPackageName(), PackageManager.GET_SIGNATURES);
+//            for (Signature signature : info.signatures) {
+//                MessageDigest md;
+//                md = MessageDigest.getInstance("SHA");
+//                md.update(signature.toByteArray());
+//                keyHash = new String(Base64.encode(md.digest(), 0));
+//                Log.d(TAG, keyHash);
+//            }
+//        } catch (Exception e) {
+//            Log.e("name not found", e.toString());
+//        }
+//        if (keyHash != null) {
+//            return keyHash;
+//        } else {
+//            return null;
+//        }
+//    }
 }
 
--- app/src/main/java/com/seok/seok/wowsup/MainActivity.java
+++ app/src/main/java/com/seok/seok/wowsup/MainActivity.java
@@ -1,14 +1,24 @@
 package com.seok.seok.wowsup;
 
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private FragmentManager fragmentManager = getSupportFragmentManager();
+
+    private FragmentProfile menu1Fragment = new FragmentProfile();
+    private Menu2Fragment menu2Fragment = new Menu2Fragment();
+    private Menu3Fragment menu3Fragment = new Menu3Fragment();
+    private Menu4Fragment menu4Fragment = new Menu4Fragment();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        // 프로필 작업 브랜치 생성 : profile
+
+
+
     }
 }
