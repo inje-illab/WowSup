576d1cd399cb1a95c2f8500c0a4d641ddd649038
msnodeve <msnodeve@gmail.com>
2019-03-22 22:36:27 +0900

    디테일 수정

--- app/src/main/java/com/seok/seok/wowsup/TranslateActivity.java
+++ app/src/main/java/com/seok/seok/wowsup/TranslateActivity.java
@@ -9,6 +9,8 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.seok.seok.wowsup.utilities.Common;
+
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
 import java.io.InputStreamReader;
@@ -17,6 +19,8 @@ import java.net.URL;
 import java.net.URLEncoder;
 
 import static com.seok.seok.wowsup.fragments.fragchat.ChatActivity.txtText;
+import static com.seok.seok.wowsup.fragments.fragprofile.StoryWriteActivity.editTextTitle;
+import static com.seok.seok.wowsup.fragments.fragprofile.StoryWriteActivity.editTextBody;
 
 public class TranslateActivity extends AppCompatActivity {
 
@@ -27,12 +31,13 @@ public class TranslateActivity extends AppCompatActivity {
     private String clientId = "_8DFZRFsspTZCrfikUzn";//애플리케이션 클라이언트 아이디값";
     private String clientSecret = "XLgmPOQ04n";//애플리케이션 클라이언트 시크릿값";
     private String strTrans;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_translate);editText = (EditText)findViewById(R.id.editText);
+        setContentView(R.layout.activity_translate);
+        editText = (EditText) findViewById(R.id.editText);
         textView = findViewById(R.id.txtTrans);
-
         btnBack = findViewById(R.id.translate_btn_back);
         btnBack.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -44,7 +49,12 @@ public class TranslateActivity extends AppCompatActivity {
         btnSendtext.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                txtText.setText(textView.getText().toString());
+                if (Common.translateOption == 1)
+                    editTextTitle.setText(editTextBody.getText().toString() + textView.getText().toString());
+                else if (Common.translateOption == 2)
+                    editTextBody.setText(editTextBody.getText().toString() + textView.getText().toString());
+                else if(Common.translateOption == 3)
+                    txtText.setText(textView.getText().toString());
                 finish();
             }
         });
@@ -53,16 +63,15 @@ public class TranslateActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 textView.setText("");
-                new Thread(){
-                    public void run()
-                    {
+                new Thread() {
+                    public void run() {
                         String strText = editText.getText().toString();
                         try {
                             String text = URLEncoder.encode(strText, "UTF-8");
                             String apiURL = "https://openapi.naver.com/v1/language/translate";
                             URL url = new URL(apiURL);
-                            Log.d("aaa",url+"");
-                            HttpURLConnection con = (HttpURLConnection)url.openConnection();
+                            Log.d("aaa", url + "");
+                            HttpURLConnection con = (HttpURLConnection) url.openConnection();
                             con.setRequestMethod("POST");
                             con.setRequestProperty("X-Naver-Client-Id", clientId);
                             con.setRequestProperty("X-Naver-Client-Secret", clientSecret);
@@ -75,7 +84,7 @@ public class TranslateActivity extends AppCompatActivity {
                             wr.close();
                             int responseCode = con.getResponseCode();
                             BufferedReader br;
-                            if(responseCode==200) { // 정상 호출
+                            if (responseCode == 200) { // 정상 호출
                                 br = new BufferedReader(new InputStreamReader(con.getInputStream()));
                             } else {  // 에러 발생
                                 br = new BufferedReader(new InputStreamReader(con.getErrorStream()));
@@ -102,11 +111,11 @@ public class TranslateActivity extends AppCompatActivity {
 
 
     }
-    public String parsing(String txtTrans)
-    {
+
+    public String parsing(String txtTrans) {
         String strReturn = txtTrans;
         String array1[] = strReturn.split(":");
-        String array2[] =array1[6].split("\"");
+        String array2[] = array1[6].split("\"");
         return array2[1];
     }
 }
